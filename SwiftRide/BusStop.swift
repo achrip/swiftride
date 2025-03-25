b417acec63886e5e4988ed4bc4cbd4f0df93211f
Ashraf Alif Adillah <ashraf.alif@gmail.com>
2025-03-25 20:40:53 +0700

    add json parsing to get bus stop data from file
    hope this works actually

--- SwiftRide/BusStop.swift
+++ SwiftRide/BusStop.swift
@@ -1,15 +1,42 @@
-import SwiftData
+import Foundation
+import CoreLocation
+import SwiftUI
 
-struct BusStop {
-    let _id: Int64
-    var _name: String
-    var _latitude: Double
-    var _longitude: Double
+struct BusStop: Identifiable, Decodable{
+    let id: UUID
+    var name: String
+    let coordinate: CLLocationCoordinate2D
+    let color: Color? 
     
-    init(_id: Int64, _name: String, _latitude: Double, _longitude: Double) {
-        self._id = _id
-        self._name = _name
-        self._latitude = _latitude
-        self._longitude = _longitude
+    enum CodingKeys: String, CodingKey {
+        case name
+        case latitude, longitude
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        id = UUID()
+        name = try container.decode(String.self, forKey: .name)
+        let latitude = try container.decode(Double.self, forKey: .latitude)
+        let longitude = try container.decode(Double.self, forKey: .longitude)
+        coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        color = nil
+    }
+}
+
+private func loadBusStops() -> [BusStop] {
+    guard let url = Bundle.main.url(forResource: "Stops", withExtension: "json") else {
+        print("JSON file not found")
+            return []
+    }
+
+    do {
+        let data = try Data(contentsOf: url)
+            let decoder = JSONDecoder()
+            let stops = try decoder.decode([BusStop].self, from: data)
+            return stops
+    } catch {
+        print("Error decoding JSON: \(error)")
+            return []
     }
 }

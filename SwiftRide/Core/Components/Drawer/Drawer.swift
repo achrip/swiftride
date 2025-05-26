98b0d0f4d209dd9f2d02aa006c02200f184ad69c
Ashraf Alif Adillah <ashraf.alif@gmail.com>
2025-05-26 17:42:51 +0700

    RESET

---
+++ SwiftRide/Core/Components/Drawer/Drawer.swift
@@ -0,0 +1,8 @@
+//
+//  Drawer.swift
+//  SwiftRide
+//
+//  Created by Ashraf Alif Adillah on 26/05/25.
+//
+
+import Foundation
---
+++ SwiftRide/Core/Helper/UIScreenHelpers.swift
@@ -0,0 +1,8 @@
+//
+//  UIScreenHelpers.swift
+//  SwiftRide
+//
+//  Created by Ashraf Alif Adillah on 26/05/25.
+//
+
+import Foundation

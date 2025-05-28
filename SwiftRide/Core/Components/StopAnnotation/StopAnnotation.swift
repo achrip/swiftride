fde320d9f2d9f4759a4ea70718fe1fb5213023f9
Ashraf Alif Adillah <ashraf.alif@gmail.com>
2025-05-28 23:34:52 +0700

    fixup! fix mapview so it displays all stops

--- SwiftRide/Core/Components/StopAnnotation/StopAnnotation.swift
+++ SwiftRide/Core/Components/StopAnnotation/StopAnnotation.swift
@@ -2,7 +2,31 @@ import MapKit
 import SwiftUI
 
 struct StopAnnotation: View {
+    let stop: Stop
+
+    init(stop: Stop) {
+        self.stop = stop
+    }
+
     var body: some View {
+        VStack {
+            ZStack {
+                Rectangle()
+                    .fill(Color.black)
+                    .frame(width: 40, height: 40)
+                    .clipShape(RoundedRectangle(cornerRadius: 12))
+                Image(systemName: "bus")
+                    .resizable()
+                    .scaledToFill()
+                    .foregroundStyle(.green)
+                    .frame(width: 25, height: 25)
+            }
+            Image(systemName: "arrowtriangle.down.fill")
+                .foregroundColor(.black)
+                .font(.system(size: 10, weight: .black))
+                .offset(x: 0, y: -5)
+        }
+        .compositingGroup()
     }
 }
 
